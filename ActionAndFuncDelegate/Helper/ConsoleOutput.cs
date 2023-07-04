6e43dc12558eda39b10966f56b6d82ec45c2c095
Tobby Umoh <tumoh@axxess.com>
2023-07-04 23:41:46 +0100

    Add project files.

---
+++ ActionAndFuncDelegate/Helper/ConsoleOutput.cs
@@ -0,0 +1,26 @@
+﻿namespace ActionAndFuncDelegate.Helper
+{
+	public class ConsoleOutput : IDisposable
+	{
+		private readonly StringWriter _consoleOutputWriter;
+		private readonly TextWriter _originalOutputWriter;
+
+		public ConsoleOutput()
+		{
+			_consoleOutputWriter = new StringWriter();
+			_originalOutputWriter = Console.Out;
+			Console.SetOut(_consoleOutputWriter);
+		}
+
+		public string GetOutput()
+		{
+			return _consoleOutputWriter.ToString();
+		}
+
+		public void Dispose()
+		{
+			Console.SetOut(_originalOutputWriter);
+			_consoleOutputWriter.Dispose();
+		}
+	}
+}
